37d96604b72673acc95d3693b2581faafbabf0db
Gabriel Gomes <ggomes@ness.com.br>
2021-07-04 20:05:54 -0300

    :fire: - primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BootstrapModule } from './bootstrap/bootstrap.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BootstrapModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
